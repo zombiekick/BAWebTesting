7153563aba22dc4b95d2bed4e0abd7b7890183ff
Zombiekick <gregorylrd@gmail.com>
2020-05-29 12:40:05 -0400

    reduced image size

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ function SetupAnim(className, controller)
     for (var i=0; i<revealElements.length; i++) { // create a scene for each element
         new ScrollMagic.Scene({
                             triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
-                            offset: 50,												 // start a little later
+                            offset: 70,												 // start a little later
                             triggerHook: 0.9,
                         })
                         .setClassToggle(revealElements[i], "visible") // add class toggle
-                        /*.addIndicators({name: className + " " + (i+1) }) // add indicators (requires plugin)*/
+                        .addIndicators({name: className + " " + (i+1) }) // add indicators (requires plugin)
                         .addTo(controller);
     }
 }
