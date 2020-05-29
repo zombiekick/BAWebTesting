5449fe12cd50cb7627bcf60ad619b01a7c5fbcd5
Zombiekick <gregorylrd@gmail.com>
2020-05-29 15:42:30 -0400

    reduced image size further

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 var scrollMagicController = new ScrollMagic.Controller();
 
-
-
 function SetupAnim(className, controller)
 {
     var revealElements = document.getElementsByClassName(className);
@@ -21,39 +19,3 @@ SetupAnim("left-anim", scrollMagicController);
 SetupAnim("right-anim", scrollMagicController);
 SetupAnim("down-anim", scrollMagicController);
 SetupAnim("up-anim", scrollMagicController);
-
-/*var controller = new ScrollMagic.Controller();
-var tl = new TimelineMax();
-tl.staggerFrom(["#section-one h1", "#section-one p"], 1.25, {
-    scale: 0,
-    cycle: {
-    y: [-50, 50]
-  },
-  ease: Elastic.easeOut,
-  stagger: {
-    from: "center",
-    amount: 0.25,
-  }
-});
-
-var scene = new ScrollMagic.Scene({
-  triggerElement: "#logo",
-  triggerHook: 0
-})
-  .addIndicators({
-    colorTrigger: "white",
-    colorStart: "white",
-    colorEnd: "white",
-    indent: 5
-  })
-  .setTween(tl)
-  .addTo(controller);
-*/
-
-/*
-AOS.init({
-    offset: 400, // offset (in px) from the original trigger point
-    delay: 0, // values from 0 to 3000, with step 50ms
-    duration: 1000 // values from 0 to 3000, with step 50ms
-  });
-  */
